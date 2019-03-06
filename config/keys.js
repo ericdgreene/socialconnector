dedc82e5afccdf3ce9493fadc2d79111d85968a4
Eric D. Greene <ericdgreene@gmail.com>
2019-03-06 15:20:49 -0800

    Push to github

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://devuser:devpassword1@ds019254.mlab.com:19254/devconnector'
+  mongoURI: 'mongodb://devuser:devpassword1@ds019254.mlab.com:19254/devconnector',
+  secretOrKey: '$ecret!'
 }
